a0afd0284b355558d7851c88646c71da0ca89f93
Vimal Gohil <gohil.vml2008@gmail.com>
2022-11-10 15:58:49 +0530

    Some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StateAddFormComponent } from './components/state-add-form/state-add-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StateAddFormComponent
-  ],
+  declarations: [AppComponent, StateAddFormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
